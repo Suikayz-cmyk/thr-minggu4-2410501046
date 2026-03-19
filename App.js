dbdaed0e29262993bb42e57da810e3584985ad15
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-03-19 17:21:06 +0700

    feat: implement add transaction screen with local state and dispatch

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { WalletProvider } from './src/context/WalletContext';
 import HomeScreen from './src/screens/HomeScreen';
+import AddTransactionScreen from './src/screens/AddTransactionScreen';
 
 export default function App() {
   return (
     <WalletProvider>
-      <HomeScreen />
+      <AddTransactionScreen />
     </WalletProvider>
   );
 }
--- src/screens/AddTransactionScreen.js
+++ src/screens/AddTransactionScreen.js
@@ -1,9 +1,63 @@
-import { View, Text } from 'react-native';
+import { useState, useContext } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { WalletContext, ACTIONS } from '../context/WalletContext';
 
 export default function AddTransactionScreen() {
+
+  //Local State
+  const [type, setType] = useState('income'); // 'income' atau 'expense'
+  const [amount, setAmount] = useState('');
+  const [note, setNote] = useState('');
+
+  //Global State
+  const { dispatch } = useContext(WalletContext);
+
+  const handleAdd = () => {
+    if (!amount) return;
+
+    const newTransaction = {
+      id: Date.now().toString(),
+      type,
+      amount: Number(amount),
+      note,
+      date: new Date().toISOString()
+    };
+
+    if (type === 'income') {
+      dispatch({ type: ACTIONS.ADD_INCOME, payload: newTransaction });
+    } else {
+      dispatch({ type: ACTIONS.ADD_EXPENSE, payload: newTransaction });
+    }
+    
+    // Reset form
+    setAmount('');
+    setNote('');
+  };
+
   return (
     <View>
-      <Text>Add Transaction Screen</Text>
+      <Text>Add Transaction</Text>
+
+      {/* Type */}
+      <Button title="Income" onPress={() => setType('income')} />
+      <Button title="Expense" onPress={() => setType('expense')} />
+
+      {/* AMOUNT */}
+      <TextInput
+        placeholder="Amount"
+        value={amount}
+        onChangeText={setAmount}
+        keyboardType="numeric"
+      />
+
+      {/* NOTE */}
+      <TextInput
+        placeholder="Note (e.g. Ayah / Belanja)"
+        value={note}
+        onChangeText={setNote}
+      />
+
+      <Button title="Add" onPress={handleAdd} />
     </View>
   );
 }
