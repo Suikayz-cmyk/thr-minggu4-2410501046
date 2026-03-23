2afeda4504696a86c4e55d7958864157bef46ecb
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-03-23 23:05:05 +0700

    feat: implement async storage for persistent transactions

--- src/context/WalletContext.js
+++ src/context/WalletContext.js
@@ -1,4 +1,6 @@
-import react, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect, useRef } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 // Action Types
 export const ACTIONS = {
@@ -58,6 +60,47 @@ export const WalletContext = createContext();
 //Provider 
 export function WalletProvider({ children }) {
     const [state, dispatch] = useReducer(walletReducer, initialState);
+    const isLoaded = useRef(false);
+
+        //Save Data ke AsyncStorage 
+       useEffect(() => {
+        if (!isLoaded.current) return; 
+
+        const saveData = async () => {
+            try {
+            await AsyncStorage.setItem(
+                'transactions',
+                JSON.stringify(state.transactions)
+            );
+            } catch (e) {
+            console.log('Error saving data', e);
+            }
+        };
+
+        saveData();
+        }, [state.transactions]);
+
+        //Load Data dari AsyncStorage 
+       useEffect(() => {
+        const loadData = async () => {
+            try {
+            const storedData = await AsyncStorage.getItem('transactions');
+
+            if (storedData !== null) {
+                dispatch({
+                type: ACTIONS.SET_TRANSACTIONS,
+                payload: JSON.parse(storedData)
+                });
+            }
+
+            isLoaded.current = true;
+            } catch (e) {
+            console.log('Error loading data', e);
+            }
+        };
+
+        loadData();
+        }, []);
 
     return (
         <WalletContext.Provider value={{ state, dispatch }}>
